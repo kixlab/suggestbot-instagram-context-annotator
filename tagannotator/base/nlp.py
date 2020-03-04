0d68a70d237bc20b66aa6b834697a4c5cc1f4524
Hyunwoo Kim <khw0726@kaist.ac.kr>
2020-03-04 21:14:37 +0900

    fix object were not working

--- tagannotator/base/nlp.py
+++ tagannotator/base/nlp.py
@@ -10,7 +10,7 @@ import wordninja
 embedding_dict = {}
 clf = None
 
-contexts = ['activity', 'emotion', 'event', 'location', 'mood', 'objects', 'time'] 
+contexts = ['activity', 'emotion', 'event', 'location', 'mood', 'object', 'time'] 
 stop_words = []
 
 def initialize():
